301377bafe1299e7f647687f2b24f4d9417e7c0b
Eric Hare <ericrhare@gmail.com>
2018-01-05 09:37:16 -0600

    New scraping code

--- scripts/scrape.R
+++ scripts/scrape.R
@@ -2,71 +2,86 @@ library(rvest)
 library(dplyr)
 library(stringr)
 
-url <- "https://pokemongo.gamepress.gg/raid-boss-counters"
-my_nodes <- url %>%
+base_url <- "https://pokemongo.gamepress.gg"
+url <- file.path(base_url, "raid-boss-counters")
+
+raid_bosses <- url %>%
     read_html() %>%
-    html_nodes(css = ".field--name-field-title , #block-gamepressbase-content .field--name-title , .raid-boss-pokemon-title a") %>%
-    html_text()
+    html_nodes(css = ".views-field-path a")
+raid_bosses_url <- paste0(base_url, html_attr(raid_bosses, "href"))
+raid_bosses_names <- gsub(" \n", "", html_text(raid_bosses))
 
-my_quick_moves <- url %>%
-    read_html() %>%
-    html_nodes(css = ".raid-pokemon-quick-move a , .raid-pokemon+ th") %>%
-    html_text()
+mydf <- data.frame(name = raid_bosses_names, url = raid_bosses_url)
 
-my_charge_moves <- url %>%
-    read_html() %>%
-    html_nodes(css = ".raid-pokemon-charge-move a , th+ th") %>%
-    html_text()
-
-mylist <- list()
-my_nodes[my_nodes %in% c("Supreme", "Good", "Glass", "Tank")] <- paste(my_nodes[my_nodes %in% c("Supreme", "Good", "Glass", "Tank")], "Counters")
-my_nodes <- my_nodes[!(my_nodes %in% c("Mawile", "Absol"))]
-for (i in 6:length(my_nodes)) {
-    if (str_count(my_nodes[i], "Supreme Counters") == 1 || str_count(my_nodes[i], "Supreme Counter") == 1) {
-        boss <- my_nodes[i - 1]
-        if (length(mylist) > 1) mylist <- mylist[1:(length(mylist) - 1)]
+myres <- apply(mydf, 1, function(x) {
+    cat(x[1], "\n")
+    
+    thepage <- x[2] %>%
+        read_html()
+    
+    counters <- thepage %>%
+        html_nodes(css = "#block-gamepressbase-content .field--label-hidden.field__item , #block-gamepressbase-content h2") %>%
+        html_text()
+    
+    fast_moves <- thepage %>%
+        html_nodes(css = ".raid-pokemon-quick-move a , .raid-pokemon+ th") %>%
+        html_text()
+    
+    charge_moves <- thepage %>%
+        html_nodes(css = ".paragraph--view-mode--default th+ th , .raid-pokemon-charge-move a") %>%
+        html_text()
+    
+    myvec <- gsub("\n", "", counters)
+    myvec <- myvec[grep("VS |Vs ", myvec, invert = TRUE)]
+    
+    mylist <- list()
+    for (i in 1:length(myvec)) {
+        if (str_count(myvec[i], "Counters") == 1 || str_count(myvec[i], "Counter") == 1) {
+            type <- strsplit(myvec[i], " ")[[1]][1]
+        } else {
+            poke <- myvec[i]
+            mylist[[length(mylist) + 1]] <- c(x[1], poke, type)
+        }
     }
-
-    if (str_count(my_nodes[i], "Counters") == 1 || str_count(my_nodes[i], "Counter") == 1) {
-        type <- strsplit(my_nodes[i], " ")[[1]][1]
-    } else {
-        poke <- my_nodes[i]
-        mylist[[length(mylist) + 1]] <- c(boss, poke, type)
+    
+    newdf <- as.data.frame(do.call(rbind, mylist), stringsAsFactors = FALSE)
+    names(newdf) <- c("Boss", "Counter", "Type")
+    
+    pokemon_ind <- 0
+    mylist_quick <- list()
+    for (i in 1:length(fast_moves)) {
+        if (fast_moves[i] == "Fast Move") {
+            pokemon_ind <- pokemon_ind + 1
+        } else {
+            poke_list <- c(unlist(newdf[pokemon_ind,]), Quick = fast_moves[i])
+            mylist_quick[[length(mylist_quick) + 1]] <- poke_list
+        }
     }
-}
-
-mydf <- as.data.frame(do.call(rbind, mylist), stringsAsFactors = FALSE)
-names(mydf) <- c("Boss", "Counter", "Type")
-
-pokemon_ind <- 0
-mylist_quick <- list()
-for (i in 1:length(my_quick_moves)) {
-    if (my_quick_moves[i] == "Fast Move") {
-        pokemon_ind <- pokemon_ind + 1
-    } else {
-        poke_list <- c(unlist(mydf[pokemon_ind,]), Quick = my_quick_moves[i])
-        mylist_quick[[length(mylist_quick) + 1]] <- poke_list
+    
+    mydf_quick <- as.data.frame(do.call(rbind, mylist_quick), stringsAsFactors = FALSE)
+    names(mydf_quick) <- c("Boss", "Counter", "Type", "Fast Attack")
+    
+    pokemon_ind <- 0
+    mylist_charge <- list()
+    for (i in 1:length(charge_moves)) {
+        if (charge_moves[i] == "Charge Move") {
+            pokemon_ind <- pokemon_ind + 1
+        } else {
+            poke_list <- c(unlist(newdf[pokemon_ind,]), Charge = charge_moves[i])
+            mylist_charge[[length(mylist_charge) + 1]] <- poke_list
+        }
     }
-}
+    
+    mydf_charge <- as.data.frame(do.call(rbind, mylist_charge), stringsAsFactors = FALSE)
+    names(mydf_charge) <- c("Boss", "Counter", "Type", "Charged Attack")
+    
+    mydf_merge <- full_join(mydf_quick, mydf_charge) %>%
+        filter(!duplicated(.))
+    
+    return(mydf_merge)
+})
 
-mydf_quick <- as.data.frame(do.call(rbind, mylist_quick), stringsAsFactors = FALSE)
-names(mydf_quick) <- c("Boss", "Counter", "Type", "Fast Attack")
+final_scrape <- do.call(rbind, myres)
+rownames(final_scrape) <- NULL
 
-pokemon_ind <- 0
-mylist_charge <- list()
-for (i in 1:length(my_charge_moves)) {
-    if (my_charge_moves[i] == "Charge Move") {
-        pokemon_ind <- pokemon_ind + 1
-    } else {
-        poke_list <- c(unlist(mydf[pokemon_ind,]), Charge = my_charge_moves[i])
-        mylist_charge[[length(mylist_charge) + 1]] <- poke_list
-    }
-}
-
-mydf_charge <- as.data.frame(do.call(rbind, mylist_charge), stringsAsFactors = FALSE)
-names(mydf_charge) <- c("Boss", "Counter", "Type", "Charged Attack")
-
-mydf_merge <- full_join(mydf_quick, mydf_charge) %>%
-    filter(!duplicated(.))
-
-write.csv(mydf_merge, file = "data/raid_counters.csv", row.names = FALSE)
+write.csv(final_scrape, file = "data/raid_counters.csv", row.names = FALSE)
